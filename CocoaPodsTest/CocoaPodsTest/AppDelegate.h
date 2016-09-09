0e1e1432380dc9f4fb366a5392dbd4a2bbe498cd
xueyu <xueyu@2dfire.com>
2016-09-09 11:37:03 +0800

    test1

---
+++ CocoaPodsTest/CocoaPodsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoaPodsTest
+//
+//  Created by guopin on 16/9/9.
+//  Copyright © 2016年 ximi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
